ecf8513da1f42cbb1a0fb7f893bd4975523dd765
Nazeh85 <160012102+Nazeh85@users.noreply.github.com>
2024-03-27 23:07:56 +0100

    Implemented coin tossing animation

---
+++ play-round/play-round.ino
@@ -0,0 +1,20 @@
+void playRound(char *choice) {
+lcd.clear(); // Rensa LCD-skärmen
+lcd.print("Tossin' coin."); // Skriv ut "Tossin' coin." på LCD-skärmen
+delay(300); // Vänta i 300 millisekunder
+lcd.clear();
+lcd.print("Tossin' coin .");
+delay(300);
+lcd.clear();
+lcd.print("Tossin' coin  .");
+delay(300);
+lcd.clear();
+lcd.print("Tossin' coin  .");
+delay(300);
+lcd.clear();
+lcd.print("Tossin' coin . ");
+delay(300);
+lcd.clear();
+lcd.print("Tossin' coin. ");
+delay(300);
+lcd.clear();
