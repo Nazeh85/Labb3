b5b72082c3cea1d8401a06e2fce01a4604c214e8
Nazeh85 <160012102+Nazeh85@users.noreply.github.com>
2024-03-27 22:27:15 +0100

    Initialized LCD screen, obtained player name, and displayed game status

--- setup-configuration/setup-configuration.ino
+++ setup-configuration/setup-configuration.ino
@@ -8,3 +8,15 @@ void setup() {
   pinMode(btnExit, INPUT_PULLUP);
   pinMode(ledWin, OUTPUT);
   pinMode(ledLoss, OUTPUT);
+
+   // Initialiserar LCD-skärmen
+  lcd.begin(16, 2);
+
+  // Anropar funktionen för att få spelarens namn
+  getPlayerName();
+
+  // Rensar LCD-skärmen och visar spelstatus, väntar 2 sekunder
+  lcd.clear();
+  displayStatus();
+  delay(2000);
+}
