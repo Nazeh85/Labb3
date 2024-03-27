3eb703c1ee45dd20720cc353d50000278b6fbb9a
Nazeh85 <160012102+Nazeh85@users.noreply.github.com>
2024-03-27 22:10:32 +0100

    Defined pins for buttons and LEDs

--- lcd-interface/lcd-interface.ino
+++ lcd-interface/lcd-interface.ino
@@ -2,3 +2,15 @@
 
 // Skapar ett LiquidCrystal-objekt med stiftkonfigurationen för LCD-skärmen
 LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
+
+// Definierar stift för olika knappar och lysdioder
+const int btnToss = 2;
+const int btnTailsHeads = 3;
+const int btnIncBet = 0;
+const int btnDecBet = 1;
+const int btnReset = 6;
+const int btnExit = 13;
+const int btnStartGame = 6; // OBS: Samma stift för btnReset och btnStartGame
+
+const int ledWin = 4;
+const int ledLoss = 5;
