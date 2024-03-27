3b81bc9e9e4a25de4344c2136bf26887280fd691
Nazeh85 <160012102+Nazeh85@users.noreply.github.com>
2024-03-27 22:58:31 +0100

    Implemented function to display message on LCD screen

--- display-status-message/display-status-message.ino
+++ display-status-message/display-status-message.ino
@@ -9,3 +9,11 @@ lcd.print(bet);
 lcd.print("$");
 lcd.print(" " + headsOrTail);
 }
+
+void displayMessage(const char *message) {
+// Rensa skärmen, visa spelstatus och meddelandet på LCD-skärmen
+lcd.clear();
+displayStatus();
+lcd.setCursor(0, 1);
+lcd.print(message);
+}
